2704757e20775220224c47c1846300dca729601c
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-21 00:05:46 -0800

    Refractor

---
+++ EmpUC2.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeewages;
+public class EmployeeWage {
+	public static void main(String args[]){
+		  int fullyDayHours = 8;
+		  int wagePerHour = 20;
+		  int dailyWage = (fullyDayHours*wagePerHour);
+
+		  System.out.println("employee is present and his daily wage is "+dailyWage);
+	  }
+    }
